fe2c000b649e9316e950de5758644bff6e07d25b
Maruf Alom <desk.maruf@gmail.com>
2023-09-10 10:56:09 +0600

    goroutines

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6}
+	result1 := fetchApi()
+	result2 := fetchApi()
+	result3 := fetchApi()
 
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
-	}
+	fmt.Printf("%v\n", result1)
+	fmt.Printf("%v\n", result2)
+	fmt.Printf("%v\n", result3)
+}
+
+func fetchApi() string {
+	time.Sleep(time.Second * 2)
+
+	return "Got it"
 }
